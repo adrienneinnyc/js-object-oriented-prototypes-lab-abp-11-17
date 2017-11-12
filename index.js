9624a77d523d4871ceb16c3f4aabc601cb217e30
Adrienne Johnson <adriennetjohnson@gmail.com>
2017-11-12 20:31:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 function BoardMember(name, homeState, training){
-   this.name = name;
+   constructor(){
+    this.name = name;
    this.homeState = homeState;
    this.training = training;
-
+ } 
+ 
    BoardMember.prototype.veto(){
      return 'No, I must disagree'
    }
